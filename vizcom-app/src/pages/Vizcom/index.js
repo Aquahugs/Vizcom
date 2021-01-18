ba33d4283359f69083915f995df076e5fbd8702c
kadokaelan <kadokaelan@gmail.com>
2021-01-18 00:13:56 -0500

    uhh

--- vizcom-app/src/pages/Vizcom/index.js
+++ vizcom-app/src/pages/Vizcom/index.js
@@ -1,72 +1,35 @@
 
-import Generate from "./Generate";
-import SketchToRender from "./SketchToRender";
 import './vizcom.scss'
 import React from "react";
-import {
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+
+import ToolCard from "../../Components/Common/tool-card";
 import GenerateLogo from'../../assets/generate-logo.png';
-import SrLogo from'../../assets/s2r.png';
+import SketchToRenderLogo from'../../assets/s2r.png';
+import {GENERATE, SKETCH_TO_RENDER } from "./vizcom-const.js"
 
 
 export default function Vizcom() {
 
-return (
-    <div className = "tools">
-        <div className = "tools-header">
-            <h1>Tools</h1>
+   
+    return (
+        <div className = "tools">
+            <div className = "tools-header">
+                <h1>Tools</h1>
+            </div>
+            <div className = "nav row">
+                <ToolCard 
+                    link={GENERATE.link} 
+                    name={GENERATE.name} 
+                    description={GENERATE.description}
+                    logo={GenerateLogo}
+                />
+                <ToolCard 
+                    link={SKETCH_TO_RENDER.link} 
+                    name={SKETCH_TO_RENDER.name} 
+                    description={SKETCH_TO_RENDER.description}
+                    logo={SketchToRenderLogo}
+                />
+            </div>
         </div>
-        <div className = "nav row">
-            <Link to="generate">
-                <div class="col s4 m4">
-                <div class="card">
-                    <div class="card-image">
-                        <img src={GenerateLogo}/>
-                    </div>
-                    <div className = "content-container">
-                    <div class="card-content">
-                        <h2>
-                          Generate  
-                        </h2>
-                        <p>Explore uniquely ai generated  generated inspiration ….</p>
-                    </div>
-                    </div>
-                  
-                </div>
-                </div>
-            </Link>
-
-            <Link to="sketch-to-render">
-                <div class="col s4 m4">
-                <div class="card">
-                    <div class="card-image">
-                    <img src={SrLogo}/>
-                    </div>
-                    <div className = "content-container">
-                        <div class="card-content">
-                            <h2>
-                            Sketch2Render  
-                            </h2>
-                            <p>Auto fill base values for all of your line drawings</p>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </Link>
-        </div>
-
-
-        {/* moved this to routes index.js because it was keeping the cards in the 
-        view where as Genereate and sketch2Render should be there own views
-        */}
-        {/* <Routes>
-            <Route path="/" element={<UsersIndex />} />
-            <Route path="generate" element={<Generate />} />
-            <Route path="sketch-to-render" element={<SketchToRender />} />
-        </Routes> */}
-    </div>
-);
+    );
 }
--- vizcom-app/src/routes/Route.js
+++
@@ -1,1 +0,0 @@
-// where we put that sign in redirect logic
