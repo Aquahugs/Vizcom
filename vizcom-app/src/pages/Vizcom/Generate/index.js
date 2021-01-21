b8a6bed046a8c289ca326afb18ba7704ac1db59c
unknown <jordanjaylor@gmail.com>
2021-01-20 18:12:23 -0800

    working on genereate

--- vizcom-app/src/pages/Vizcom/Generate/index.js
+++ vizcom-app/src/pages/Vizcom/Generate/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import './generate.scss';
 
 export default function Generate() {
-  return <h1>Generate</h1>;
+  return( 
+    <div className = "generate-container">
+      <div className ="row">
+      <a class="waves-effect waves-teal btn-flat btn-color">Button</a>
+      <a class=" btn-flat">Button</a>
+      </div>
+    </div>
+
+  );
 }
