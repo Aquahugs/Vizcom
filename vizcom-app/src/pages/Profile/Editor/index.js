d141071d3d1231875a804563ae264de03b9e1ad9
unknown <jordanjaylor@gmail.com>
2021-02-27 19:07:35 -0800

    pulling in latest merges

--- vizcom-app/src/pages/Profile/Editor/index.js
+++ vizcom-app/src/pages/Profile/Editor/index.js
@@ -14,14 +14,14 @@ import backarrow from "../../../assets/back-arrow.svg";
 import { ProfileThunks } from "../redux";
 import { withAuthorization } from "../../../router/auth/session";
 
-const Editor = ({ updateProfile, history, uid }) => {
+const Editor = ({ updateProfile, history, uid,profile }) => {
   const { register, handleSubmit } = useForm();
 
   const submitForm = (formData) => {
     updateProfile(formData, uid);
     history.push("/profile");
   };
-
+  console.log(profile.first_name)
   return (
     <div className="row form-container">
       <form className="col s12" onSubmit={handleSubmit(submitForm)}>
@@ -30,30 +30,32 @@ const Editor = ({ updateProfile, history, uid }) => {
             <img className="backarrow" src={backarrow} />
           </Link>
           <h1>Edit profile</h1>
+          
           <div className="input-field col s6">
-            <label htmlFor="name">First Name</label>
-            <input type="text" name="firstName" id="firstName" ref={register} />
+            <input placeholder="Placeholder" defaultValue={profile.first_name} class="validate" type="text"  name="firstName" id="firstName" ref={register} />
+            <label class="active" for="first_name">First Name</label>
           </div>
           <div className="input-field col s6">
-            <label htmlFor="name">Last Name</label>
-            <input type="text" name="lastName" id="lastName" ref={register} />
+            <label  class="active" htmlFor="name">Last Name</label>
+            <input defaultValue={profile.last_name}  for="first_name" type="text" name="lastName" id="lastName"ref={register} />
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <label htmlFor="name">Location</label>
-            <input type="text" name="location" id="location" ref={register} />
+            <label class="active"  htmlFor="name">Location</label>
+            <input defaultValue={profile.location} type="text" name="location" id="location" ref={register} />
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <label htmlFor="bio">Bio</label>
+            <label  class="active"  htmlFor="bio">Bio</label>
             <textarea
               name="bio"
               className="materialize-textarea"
               id="bio"
               ref={register}
               maxLength="150"
+              defaultValue={profile.bio}
             />
           </div>
         </div>
@@ -65,12 +67,14 @@ const Editor = ({ updateProfile, history, uid }) => {
                 <i className="material-icons prefix">
                   <img src={instaIcon} />
                 </i>
-                <label htmlFor="instagram">Instagram</label>
+                <label  class="active"  htmlFor="instagram">Instagram</label>
                 <input
+                  class="active" 
                   type="text"
                   name="instagram"
                   id="instagram"
                   ref={register}
+                  defaultValue={profile.instagram}
                 />
               </div>
             </li>
@@ -79,8 +83,11 @@ const Editor = ({ updateProfile, history, uid }) => {
                 <i className="material-icons prefix">
                   <img src={twitterIcon} />
                 </i>
-                <label htmlFor="twitter">Twitter</label>
-                <input type="text" name="twitter" id="twitter" ref={register} />
+                <label class="active"  htmlFor="twitter">Twitter</label>
+                <input 
+               
+                defaultValue={profile.twitter}
+                type="text" name="twitter" id="twitter" ref={register} />
               </div>
             </li>
             <li>
@@ -90,6 +97,8 @@ const Editor = ({ updateProfile, history, uid }) => {
                 </i>
                 <label htmlFor="personalSite">Website</label>
                 <input
+                  class="active" 
+                  defaultValue={profile.personal_site}
                   type="text"
                   name="personalSite"
                   id="personalSite"
@@ -118,6 +127,7 @@ const Editor = ({ updateProfile, history, uid }) => {
           </button>
         </div>
       </form>
+      
     </div>
   );
 };
@@ -126,6 +136,7 @@ const mapStateToProps = (state) => {
   return {
     user: state.profile.user,
     uid: state.session.authUser.uid,
+    profile: state.profile.user
   };
 };
 
