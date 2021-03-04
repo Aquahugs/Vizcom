ae69bd972998b638640a3b0b2a4b683d84494bba
unknown <jordanjaylor@gmail.com>
2021-03-03 19:55:22 -0800

    pushing up jordobranch merge

--- vizcom-app/src/common/components/SecondaryNavbar.js
+++ vizcom-app/src/common/components/SecondaryNavbar.js
@@ -4,19 +4,6 @@ import { connect } from "react-redux";
 import "./SecondaryNavbar.scss";
 import { withRouter } from 'react-router-dom';
 
-<<<<<<< HEAD
-const SecondaryNav = (props) => {
-  console.log(props)
-  const [view, setView] = useState("");
-
-  const toggleView = (e) => {
-    setView(e);
-    console.log(view);
-  };
- 
-  return(
-    <div className = "nav-container">
-=======
 
 const SecondaryNav = ({ user, profile, history }) => {
 
@@ -25,8 +12,7 @@ const SecondaryNav = ({ user, profile, history }) => {
   console.log(history.location)
   return (
     <div className="nav-container">
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
-      <ul>
+          <ul>
         <li>
           <Link to="/home">Home <span>{history.location.pathname}</span></Link>
         </li>
@@ -50,21 +36,11 @@ const SecondaryNav = ({ user, profile, history }) => {
   );
 };
 
-<<<<<<< HEAD
-const mapStateToProps = state => {
-  return{
-    user: state.session,
-    profile: state.profile.user
-
-  }
-}
-=======
 const mapStateToProps = (state) => {
   return {
     user: state.session,
     profile: state.profile.user
     
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
 
   };
 };
--- vizcom-app/src/pages/Profile/Editor/index.js
+++ vizcom-app/src/pages/Profile/Editor/index.js
@@ -14,11 +14,7 @@ import backarrow from "../../../assets/back-arrow.svg";
 import { ProfileThunks } from "../redux";
 import { withAuthorization } from "../../../router/auth/session";
 
-<<<<<<< HEAD
-const Editor = ({ updateProfile, history, uid,profile }) => {
-=======
 const Editor = ({ updateProfile, history, uid, user }) => {
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
   const { register, handleSubmit } = useForm();
   console.log(user)
 
@@ -38,52 +34,30 @@ const Editor = ({ updateProfile, history, uid, user }) => {
           <h1>Edit profile</h1>
           
           <div className="input-field col s6">
-<<<<<<< HEAD
-            <input placeholder="Placeholder" defaultValue={profile.first_name} class="validate" type="text"  name="firstName" id="firstName" ref={register} />
-            <label class="active" for="first_name">First Name</label>
-          </div>
-          <div className="input-field col s6">
-            <label  class="active" htmlFor="name">Last Name</label>
-            <input defaultValue={profile.last_name}  for="first_name" type="text" name="lastName" id="lastName"ref={register} />
-=======
           <label  class = "active" for="first_name">First Name</label>
             <input type="text" name="firstName" id="firstName" ref={register} defaultValue={user.first_name}/>
           </div>
           <div className="input-field col s6">
             <label class = "active" htmlFor="name">Last Name</label>
             <input type="text" name="lastName" id="lastName" ref={register} defaultValue={user.last_name} />
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-<<<<<<< HEAD
-            <label class="active"  htmlFor="name">Location</label>
-            <input defaultValue={profile.location} type="text" name="location" id="location" ref={register} />
-=======
             <label class = "active" htmlFor="name">Location</label>
             <input type="text" name="location" id="location" ref={register} defaultValue={user.location} />
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-<<<<<<< HEAD
-            <label  class="active"  htmlFor="bio">Bio</label>
-=======
             <label class = "active" htmlFor="bio">Bio</label>
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
             <textarea
               name="bio"
               className="materialize-textarea"
               id="bio"
               ref={register}
               maxLength="150"
-<<<<<<< HEAD
-              defaultValue={profile.bio}
-=======
               defaultValue={user.bio}
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
             />
           </div>
         </div>
@@ -95,22 +69,14 @@ const Editor = ({ updateProfile, history, uid, user }) => {
                 <i className="material-icons prefix">
                   <img src={instaIcon} />
                 </i>
-<<<<<<< HEAD
-                <label  class="active"  htmlFor="instagram">Instagram</label>
-=======
                 <label class = "active" htmlFor="instagram">Instagram</label>
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
                 <input
                   class="active" 
                   type="text"
                   name="instagram"
                   id="instagram"
                   ref={register}
-<<<<<<< HEAD
-                  defaultValue={profile.instagram}
-=======
                   defaultValue={user.instagram}
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
                 />
               </div>
             </li>
@@ -119,16 +85,8 @@ const Editor = ({ updateProfile, history, uid, user }) => {
                 <i className="material-icons prefix">
                   <img src={twitterIcon} />
                 </i>
-<<<<<<< HEAD
-                <label class="active"  htmlFor="twitter">Twitter</label>
-                <input 
-               
-                defaultValue={profile.twitter}
-                type="text" name="twitter" id="twitter" ref={register} />
-=======
                 <label class = "active" htmlFor="twitter">Twitter</label>
                 <input type="text" name="twitter" id="twitter" ref={register} defaultValue={user.twitter} />
->>>>>>> f315311d4995d49089f0ef9126c5eaf9b9fb2c93
               </div>
             </li>
             <li>
