e397dd722bae7636805c6ea117e19ad2f6ef8ad4
kadokaelan <kadokaelan@gmail.com>
2021-04-27 16:38:07 -0400

    fixed image upload

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -7,10 +7,12 @@ import { useForm } from "react-hook-form";
 import uploadService from "../../common/services/upload-service";
 import { Select } from "react-materialize";
 import ProfileThunks from "../Profile/redux/thunks";
+import { Modal, Button } from "react-materialize";
 
 export const Admin = ({ history, user, uid, getProfile }) => {
   const [files, setFiles] = useState([]);
   const [imageType, setImageType] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!user) {
@@ -34,56 +36,112 @@ export const Admin = ({ history, user, uid, getProfile }) => {
     setImageType(e.target.value);
   };
 
-  const submitForm = () => {
-    const data = new FormData();
+  const handleSubmitForm = () => {
+    setIsLoading(true);
+    const formData = new FormData();
 
     for (let i = 0; i < files.length; i += 1) {
-      data.append("file", files[i]);
+      files[i].generated_image_type = imageType;
+      formData.append("file", files[i]);
     }
+
     const req = {
       generated_image_type: imageType,
-      files,
+      formData,
     };
-    const response = uploadService.uploadImages(req);
+    uploadService.uploadImages(req).then(() => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 10000);
+    });
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit(submitForm)}>
-        <h3>Upload Images to Google Cloud</h3>
-        <Dropzone files={files} setFiles={setFiles} multiple={true} />
-        <Select
-          onChange={(e) => {
-            handleDropdownClick(e);
-          }}
-          id="Select-9"
-          label="Chose your image type"
-          multiple={false}
-          options={{
-            classes: "",
-            dropdownOptions: {
-              alignment: "left",
-              autoTrigger: true,
-              closeOnClick: true,
-              constrainWidth: true,
-              coverTrigger: true,
-              hover: false,
-              inDuration: 150,
-              outDuration: 250,
-            },
-          }}
-          value="2"
-        >
-          {/* <option value="car">Car</option> */}
-          <option value="concept">Concept</option>
-          {/* <option value="shoe">Shoe</option> */}
-        </Select>
-        <button className="btn btn-primary" type="submit" name="action">
-          Upload
-        </button>
-      </form>
-    </div>
+  const trigger = (
+    <button className="btn btn-primary" type="submit" name="action">
+      Upload
+    </button>
   );
+
+  if (isLoading) {
+    return (
+      <div>
+        <img
+          src="https://media.giphy.com/media/wnYB3vx9t6PXiq1ubB/giphy.gif"
+          alt="loading..."
+        ></img>
+        <img
+          src="https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif"
+          alt="loading..."
+        ></img>
+        <img
+          src="https://media.giphy.com/media/TlK63EVjkyLQpvsETWU/giphy.gif"
+          alt="loading..."
+        ></img>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <form enctype="multipart/form-data">
+          <h3>Upload Images to Google Cloud</h3>
+          <Dropzone files={files} setFiles={setFiles} multiple={true} />
+          <Select
+            onChange={(e) => {
+              handleDropdownClick(e);
+            }}
+            id="Select-9"
+            label="Chose your image type"
+            multiple={false}
+            options={{
+              classes: "",
+              dropdownOptions: {
+                alignment: "left",
+                autoTrigger: true,
+                closeOnClick: true,
+                constrainWidth: true,
+                coverTrigger: true,
+                hover: false,
+                inDuration: 150,
+                outDuration: 250,
+              },
+            }}
+            value="2"
+          >
+            <option value="car">Car</option>
+            <option value="concept">Concept</option>
+            <option value="shoe">Shoe</option>
+          </Select>
+        </form>
+        <Modal
+          actions={[
+            <Button
+              flat
+              modal="close"
+              node="button"
+              waves="green"
+              onClick={() => handleSubmitForm()}
+            >
+              YES
+            </Button>,
+            <Button flat modal="close" node="button" waves="light">
+              No
+            </Button>,
+          ]}
+          options={{
+            dismissible: true,
+            endingTop: "10%",
+            inDuration: 250,
+            opacity: 0.5,
+            outDuration: 250,
+            preventScrolling: true,
+            startingTop: "4%",
+          }}
+          header="DID YOU CHOSE YOUR IMAGE TYPE"
+          trigger={trigger}
+        ></Modal>
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state) => ({
