86acf93512110c51f4bb4804586b2100411b569a
unknown <jordanjaylor@gmail.com>
2021-06-27 22:13:58 -0700

    switching to main

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -7,12 +7,12 @@ import "./LandingPage.scss";
 import { Desktop, Tablet, Mobile } from "./Responsive/responsive";
 import TabletView from "./Responsive/tablet";
 import MobileView from "./Responsive/mobile";
-import WhiteLogo from "../../assets/logo.png";
+import WhiteLogo from "../../assets/logo-white.png";
 import graphic from "../../assets/creative-thinking.svg";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
-import { Button } from 'antd';
+import { Button,Card } from 'antd';
 import { Row } from 'antd';
 import { Col } from 'antd';
 import 'antd/dist/antd.css';
@@ -31,9 +31,14 @@ const LandingPage = ({ authUser, history }) => {
     <div>
       <Desktop>
         <div className="content-container2">
+          <div className = "row landing-nav-bar">
+            <img src = {WhiteLogo}/>
+            <Button className="sign-up-btn" type="primary">Get Started</Button>
+            <p>Log in</p>
+          </div>
           <div className = "row">
             <div className = "col s12 m12 l12 landing-banner">
-              <div style = {{padding:"20%",paddingBottom:'10%'}}>
+              <div className = 'intro-container'>
                 <h1>We're reimaging the way artist and designers create.</h1>
                 <p> Ai enabled creative tools that shorten the distance between having an idea and bringing it to life.</p>
                 <div className = "row intro-btn-container">
@@ -53,7 +58,13 @@ const LandingPage = ({ authUser, history }) => {
             </div>
           </div>
           <div className = "row quote">
-            <h1 style = {{color:"white"}}>Quite an interesting tool that will shape a transportation designer’s job in the near future : Selecting ideas and curating the AI’s generated design. Interesting...</h1>
+            <h1>Inspired by artist</h1>
+            <h2>Built for the future</h2>
+            <Card style={{ width: 300 }}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
           </div>
           <div className = "row quote2">
             <h2>Machines that assist humans in <br/>creative thinking.</h2>
