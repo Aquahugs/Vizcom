7bc2e97ce5c475f96340cd98c42b88057a410655
kadokaelan <kadokaelan@gmail.com>
2021-07-10 14:45:49 -0700

    redirect sketch to render beta members

--- src/common/components/SecondaryNavbar/index.js
+++ src/common/components/SecondaryNavbar/index.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import "./SecondaryNavbar.scss";
 import { Link, useLocation } from "react-router-dom";
 import { HOME, PROFILE, EXPLORE } from "../../../router/routes-const";
-import { Breadcrumb } from "antd";
 
 const SecondaryNav = ({ profile }) => {
   let location = useLocation();
@@ -17,50 +16,29 @@ const SecondaryNav = ({ profile }) => {
   return (
     <div className="nav-container">
       <ul>
-        <li className="hvr-forward ">
-          <li>
-            <Breadcrumb>
-              <Breadcrumb.Item>
-                <Link to={HOME}>Tools</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                {(location.pathname === "/generate" ||
-                  location.pathname === "/sketch-to-render" ||
-                  location.pathname === "/sketch-to-render/beta" ||
-                  location.pathname === "/home") && (
-                  <span className="hvr-forward" style={styles}>
-                    {capitalizeFirstLetter(location.pathname)}
-                  </span>
-                )}
-              </Breadcrumb.Item>
-            </Breadcrumb>
-          </li>
+        <li className=" hvr-forward">
+          <Link to={HOME}>
+            Tools
+            {(location.pathname === "/generate" ||
+              location.pathname === "/home" ||
+              location.pathname === "/sketch-to-render" ||
+              location.pathname === "/sketch-to-render/beta") && (
+              <span style={styles}>
+                /{capitalizeFirstLetter(location.pathname)}
+              </span>
+            )}
+          </Link>
         </li>
-
-        <li className="hvr-forward ">
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Link to={PROFILE}>Collection</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              {location.pathname === "/profile" && profile?.first_name && (
-                <span style={styles}>{profile?.first_name}</span>
-              )}
-            </Breadcrumb.Item>
-          </Breadcrumb>
+        <li className=" hvr-forward">
+          <Link to={PROFILE}>
+            Collection
+            {location.pathname === "/profile" && profile?.first_name && (
+              <span style={styles}>/{profile?.first_name}</span>
+            )}
+          </Link>
         </li>
-
-        <li className="hvr-forward ">
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Link to={EXPLORE}>Explore</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              {location.pathname === "/explore" && (
-                <span style={styles}>home</span>
-              )}
-            </Breadcrumb.Item>
-          </Breadcrumb>
+        <li className="hvr-forward">
+          <Link to={EXPLORE}>Explore</Link>
         </li>
       </ul>
     </div>
