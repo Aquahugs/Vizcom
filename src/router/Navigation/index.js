da956902946e662c6c47c86bfea5878b33206581
kadokaelan <kadokaelan@gmail.com>
2021-07-10 19:58:19 -0700

    final changes for google analytics and hotjar

--- src/router/Navigation/index.js
+++ src/router/Navigation/index.js
@@ -8,7 +8,9 @@ import ReactGA from "react-ga";
 import { hotjar } from "react-hotjar";
 
 const Navigation = ({ authUser }) => {
+  // google analytics and hotjar tracking
   useEffect(() => {
+    ReactGA.initialize("UA-201824980-1");
     hotjar.initialize(2497540, 6);
     if (authUser) {
       hotjar.identify("USER_ID", { userProperty: "authUser.uid" });
